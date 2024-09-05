23b38c5a6449c481ebf0dca19a9d8d0d30ec2307
Matvey <sinema3d00@gmail.com>
2024-09-05 12:43:41 +0300

    Making Interface for shapes

---
+++ ShapeCalculatingProject/Interfaces/IShape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeCalculatingProject.Interfaces
+{
+    public interface IShape
+    {
+        float CalculateArea();
+    }
+}
