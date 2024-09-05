966262edec0670fe5409f292e0dcaf5573fa2858
Matvey <sinema3d00@gmail.com>
2024-09-05 13:15:03 +0300

    made factory for easy creating objects

---
+++ ShapeCalculatingProject/Factory/ShapeFactory.cs
@@ -0,0 +1,19 @@
+﻿using ShapeCalculatingProject.Interfaces;
+using ShapeCalculatingProject.Models;
+
+
+namespace ShapeCalculatingProject.Factory
+{
+    public static class ShapeFactory
+    {
+        public static IShape CreateCircle(double radius)
+        {
+            return new Circle(radius);
+        }
+
+        public static IShape CreateTriangle(double sideA, double sideB, double sideC)
+        {
+            return new Triangle(sideA, sideB, sideC);
+        }
+    }
+}
