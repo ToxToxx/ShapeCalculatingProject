f10c56b2ccb7e66aae53aaee1d5ea87481a9b2c7
Matvey <sinema3d00@gmail.com>
2024-09-05 13:14:45 +0300

    made circle model

---
+++ ShapeCalculatingProject/Models/Circle.cs
@@ -0,0 +1,20 @@
+﻿using ShapeCalculatingProject.Interfaces;
+
+namespace ShapeCalculatingProject.Models
+{
+    public class Circle : IShape
+    {
+        public double CircleRadius { get; }
+
+        public Circle(double radius)
+        {
+            if (radius <= 0) throw new ArgumentException("Radius must be positive.");
+            CircleRadius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Math.Pow(CircleRadius, 2);
+        }
+    }
+}
