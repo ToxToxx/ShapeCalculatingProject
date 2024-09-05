547439e527d3840f643a8349e154f3cded8e3b5c
Matvey <sinema3d00@gmail.com>
2024-09-05 13:14:36 +0300

    fixing bugs

--- ShapeCalculatingProject/Interfaces/IShape.cs
+++ ShapeCalculatingProject/Interfaces/IShape.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace ShapeCalculatingProject.Interfaces
 {
     public interface IShape
     {
-        float CalculateArea();
+        double CalculateArea();
     }
 }
